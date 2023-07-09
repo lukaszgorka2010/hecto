16ee5ce5492f6aabe02cf883e7ad05d67cb6ee53
Lukasz Gorka <lukasz-gorka@air-lukasz.home>
2023-07-09 02:25:46 +0100

    using event::Key

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,27 @@
-use std::io::{self, stdout, Read};
+use std::io::{self, stdout};
+use termion::event::Key;
+use termion::input::TermRead;
 use termion::raw::IntoRawMode;
-
-fn to_control_byte(c: char) -> u8 {
-let byte = c as u8;
-byte & 0b0001_1111
-}
 
 fn die(e: std::io::Error){
     panic!("{}", e);
 }
 fn main() {
     let _stdout = stdout().into_raw_mode().unwrap();
-    for b in io::stdin().bytes() {
-        match b {
-            Ok(b) => {
-                let c = b as char;
-                if c.is_control() {
-                    println!("{:?} \r", b);
-                } else {
-                    println!("{:?} ({})\r", b, c);
+    for key in io::stdin().keys() {
+        match key {
+            Ok(key) => match key {
+                Key::Char(c) =>{
+                    if c.is_control() {
+                        println!("{:?} \r", c as u8);
+                    } else {
+                        println!("{:?} ({})\r", c as u8, c);
+                    }
                 }
-                if b == to_control_byte('c') {
-                    break;
-                }
-            }
-            Err(err) => {
-                die(err)
-            }
+                Key::Ctrl('c') => break,
+                _ => println!("{:?}\r", key),
+            },
+            Err(err) => die(err),
         }
     }
 }
