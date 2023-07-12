abb6b8d781aa21b982125184873cf6f8e95c92e0
lukaszgorka2010 <lukaszgorka2010@gmail.com>
2023-07-12 18:16:43 +0100

    bug fixed

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@
 mod editor;
 mod terminal;
 pub use terminal::Terminal;
-
 use editor::Editor;
+pub use editor::Position;
 
 fn main() {
     Editor::default().run();
--- src/terminal.rs
+++ src/terminal.rs
@@ -1,3 +1,4 @@
+use crate::Position;
 use std::io::{self, stdout, Write};
 use termion::event::Key;
 use termion::input::TermRead;
@@ -29,15 +30,17 @@ impl Terminal {
     pub fn clear_screen() {
         print!("{}", termion::clear::All);
     }
-    pub fn cursor_position(x: u16, y: u16) {
-        let x = x.saturating_add(1);
-        let y = y.saturating_add(1);
+    pub fn cursor_position(postion: &Position){
+        let Position { mut x, mut y } = postion;
+        x = x.saturating_add(1);
+        y = y.saturating_add(1);
+        let x = x as u16;
+        let y = y as u16;
         print!("{}", termion::cursor::Goto(x,y));
     }
     pub fn flush() -> Result<(), std::io::Error>{
         io::stdout().flush()
     }
-
     pub fn read_key() -> Result<Key, std::io::Error> {
         loop {
             if let Some(key) = io::stdin().lock().keys().next() {
@@ -45,5 +48,13 @@ impl Terminal {
             }
         }
     }
-
+    pub fn cursor_hide() {
+        print!("{}", termion::cursor::Hide);
+    }
+    pub fn cursor_show() {
+        print!("{}", termion::cursor::Show);
+    }
+    pub fn clear_current_line() {
+        print!("{}", termion::clear::CurrentLine);
+    }
 }
